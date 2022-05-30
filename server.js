445b7f0860c432f71d3576969399ceabb701c9d1
hassanaref <hassan.aref159@gmail.com>
2022-05-30 11:00:12 +0200

    setup morgan logger middleware

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require('./middleware/logger')
+const morgan = require('morgan')
 // env config 
 dotenv.config({ path: "./config/config.env" });
 // route files
 const bootcamps = require("./routes/bootcamps.js");
 const app = express();
-app.use(logger)
+// dev logger tool
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 // mount routers
 app.use("/v1/bootcamps", bootcamps);
 const port = process.env.PORT || 5000;
